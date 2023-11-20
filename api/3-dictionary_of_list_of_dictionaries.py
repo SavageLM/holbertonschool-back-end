e52a85c513f7d9ec0c601311da66b43200066b87
SavageLM <nagol.egavas@gmail.com>
2023-11-20 11:16:38 -0600

    Updated for loop if condition and variable for pulling data

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -17,17 +17,17 @@ def all_to_JSON():
     data = {}
     for task in todo_data:
         # Getting User ID
-        user_id = task['userID']
+        user_id = task['userId']
         # Pulling User Data
         user_data = requests.get(f"{API_URL}/users/{user_id}").json()
         # Checking if user is already in Data dict
-        if data[user_id] not in data:
+        if user_id not in data:
             data[user_id] = []
         # Adding todo data to UserID list
         data[user_id].append({
             "username": user_data['username'],
-            "task": todo_data['title'],
-            "completed": todo_data['completed']
+            "task": task['title'],
+            "completed": task['completed']
         })
     # Creating file to write to
     with open("todo_all_employees.json", 'w') as file:
